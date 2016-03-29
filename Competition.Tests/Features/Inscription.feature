be17c44ec0c6439dc6c41863d91f35bca54e0e22
Eric De Carufel <edecarufel@pyxis-tech.com>
2016-03-28 21:46:47 -0400

    Initial commit

---
+++ Competition.Tests/Features/Inscription.feature
@@ -0,0 +1,11 @@
+﻿# language en
+Feature: Inscription
+	Afin de démarrer une compétition
+	En tant qu'organisateur
+	Je veux inscrire des partipants
+
+Scenario: Inscription d'un nouveau participant
+	Given Jow Blow n'est pas inscrit
+	When Jow Blow s'inscrit
+	Then Il doit fournir son "numéro de terrain"
+
---
+++ Competition.Tests/Steps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Competition.Lib;
+using TechTalk.SpecFlow;
+
+namespace Competition.Tests
+{
+    [Binding]
+    public class Steps
+    {
+        [Given(@"Jow Blow n'est pas inscrit")]
+        public void GivenJowBlowNEstPasInscrit()
+        {
+            
+        }
+        
+        [When(@"Jow Blow s'inscrit")]
+        public void WhenJowBlowSInscrit()
+        {
+            var database = Database.Load();
+            database.Save(new Personne()
+            {
+                Prenom = "Joe", 
+                Nom = "Blow",
+                NumeDeTerrain = 103
+            });
+        }
+
+        [Then(@"Il doit fournir son ""(.*)""")]
+        public void ThenIlDoitFournirSon(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
